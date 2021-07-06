1bb3d36a552b6d2a2894ac90069f89858cc660ce
sushilv3 <sushilv3rma@gmail.com>
2021-07-06 12:46:34 +0530

    exam portal web services project setup

---
+++ src/main/java/com/kodecamp/examwebservices/ExamportalwebservicesApplication.java
@@ -0,0 +1,14 @@
+package com.kodecamp.examwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamportalwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamportalwebservicesApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/kodecamp/examwebservices/ExamportalwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodecamp.examwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamportalwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
